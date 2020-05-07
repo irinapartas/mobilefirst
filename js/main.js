962037c200d7127102127d7a51008be50229efa8
Ira <ilovebellsdaya@yandex.ru>
2020-05-07 16:20:30 +0300

    add slider js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
 $(function(){
-
+    $('.responsive').slick({
+        dots: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+            }
+          }
+        
+        ]
+      });
 });
