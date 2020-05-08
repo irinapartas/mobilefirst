0316a7d8c39af292f4f7f7a5904f6731cdbbf784
Ira <ilovebellsdaya@yandex.ru>
2020-05-08 13:52:07 +0300

    two blocks

--- js/main.js
+++ js/main.js
@@ -1,25 +1,23 @@
 $(function(){
-    $('.responsive').slick({
+    $('.top-slider').slick({
         dots: true,
         slidesToShow: 1,
         slidesToScroll: 1,
-        nextArrow: "<button type="button" class="slick-arrow slick-next"><img src="images/next-arrow.svg" alt="Next Arrow"></button>",
-        prevArrow: "<button type="button" class="slick-arrow slick-prev"><img src="images/next-arrow.svg" alt="Prev Arrow"></button>",        
+        nextArrow: <button type="button" class="slick-arrow" class="slick-next" img src="images/next-arrow.svg" alt="next arrow"></button>,
+        prevArrow: <button type="button" class="slick-arrow" class="slick-next" img src="images/prev-arrow.svg" alt="prev arrow"></button>,
         responsive: [
           {
             breakpoint: 1024,
             settings: {
+              
             }
-          },
-          
-          
+          },        
           {
             breakpoint: 640,
             settings: {
                 arrows: false,
             }
           }
-        
         ]
-      });
+    });
 });
